6ff0ca166b4165b2295187a02483695cdf3b6965
Yevhenbk <balagutrak.jenia@gmail.com>
2024-03-13 01:03:28 +0100

    add ask ai

--- src/components/AskAIButton.tsx
+++ src/components/AskAIButton.tsx
@@ -1,8 +1,14 @@
 import { FC } from "react"
+import { SiWorldhealthorganization } from "react-icons/si"
+import Link from "next/link"
 
 const AskAIButton: FC = () => {
   return (
-    <div>AskAIButton</div>
+    <Link className="border-[1px] border-secondaryGrey flex justify-between
+    py-3 px-6 hover:cursor-pointer hover:bg-secondaryGrey rounded-md" href="/">
+      Ask AI
+      <SiWorldhealthorganization className="text-2xl" />
+    </Link>
   )
 }
 
--- src/components/chatbot/ChatList.tsx
+++ src/components/chatbot/ChatList.tsx
@@ -19,6 +19,7 @@ const ChatList: FC<ChatListProps> = ({chatList, createChat}) => {
       <div className="flex flex-col h-[85dvh] overflow-y-scroll w-full">
         <ChatForm createChat={createChat} />
         <div className="flex flex-col gap-3 mb-12">
+          <AskAIButton />
           {chatList && chatList.length > 0 ? (
             chatList.map((chat: ChatData) => (
               <ChatHeader key={chat.id} id={chat.id} href={`/chat/${chat.id}`}>
