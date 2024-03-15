c0822a2b534d8b02e279a663d907922f4587b39c
Yevhenbk <balagutrak.jenia@gmail.com>
2024-03-15 23:26:38 +0100

    add modal state

--- src/components/chatbot/SubmitMessage.tsx
+++ src/components/chatbot/SubmitMessage.tsx
@@ -3,7 +3,6 @@
 import { FC, useState, ChangeEvent } from "react"
 import Input from "../Input"
 import Button from "../Button"
-import { useModalState } from "../hooks/useModalState"
 import { MdOutlineSubdirectoryArrowLeft } from "react-icons/md"
 
 interface SubmitMessageProps {
@@ -16,22 +15,26 @@ interface SubmitMessageProps {
 const SubmitMessage: FC<SubmitMessageProps> = ({ id, handleSubmit }) => {
   const [inputText, setInputText] = useState<string>("")
   const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(true)
-  const [modal, setModal] = useModalState()
+  const [modal, setModal] = useState<boolean>(false)
 
-  const handleSendClick = (e: any) => {
+  const handleSendClick = async (e: any) => {
     e.preventDefault()
     if (inputText.trim() !== "") {
       const splitInput = inputText.split(",")
       const medicationName = splitInput[0].trim()
       const medicationPurpose = splitInput[1]?.trim() || "purpose"
 
-      handleSubmit?.(id, medicationName, medicationPurpose)
-      setInputText("")
-      setIsButtonDisabled(true)
-      setModal(false)
+      try {
+        await handleSubmit?.(id, medicationName, medicationPurpose)
+        setInputText("")
+        setIsButtonDisabled(true)
+        setModal(false)
+      } catch (error) {
+        console.error("Error sending message:", error)
+        setModal(true)
+      }
     } else {
       console.log("Please enter a non-empty input.")
-      setModal(true)
     }
   }
 
