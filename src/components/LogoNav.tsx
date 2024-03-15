334d3e18cd5207600b41b1399b39b6d7c38f5742
Yevhenbk <balagutrak.jenia@gmail.com>
2024-03-15 21:57:21 +0100

    add logo nav

--- src/components/LogoNav.tsx
+++ src/components/LogoNav.tsx
@@ -6,7 +6,7 @@ interface LogoNavInterface {
   logo: string
 }
 
-const LogoNav: FC<LogoNavInterface> = ({logo}) => {
+const LogoNav: FC<LogoNavInterface> = ({ logo }) => {
   return (
     <nav className="left-0 top-8 w-full px-6 sm:absolute md:top-[22px] md:px-6 lg:px-8 hidden md:flex">
       <Link className="flex cursor-default items-center text-[20px] font-bold leading-none lg:text-[22px] gap-2
