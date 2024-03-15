1b4578eb9b5cd523fc11211330d89be4638718d5
Yevhenbk <balagutrak.jenia@gmail.com>
2024-03-15 23:26:02 +0100

    remove use modal state

--- src/components/hooks/useModalState.tsx
+++
@@ -1,7 +0,0 @@
-import { useState, Dispatch, SetStateAction } from "react"
-
-export const useModalState = (): [boolean, Dispatch<SetStateAction<boolean>>] => {
-  const [modal, setModal] = useState<boolean>(false)
-
-  return [modal, setModal]
-}
