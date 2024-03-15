39e1817a2b9a9307a11d47621cea2724e4414c41
Yevhenbk <balagutrak.jenia@gmail.com>
2024-03-15 17:59:20 +0100

    add props

--- src/components/Caption.tsx
+++ src/components/Caption.tsx
@@ -1,13 +1,20 @@
 import { FC} from "react"
 import Link from "next/link"
+import { Url } from "next/dist/shared/lib/router/router"
 
-const Caption: FC = () => {
+interface CaptionProps {
+  link: Url
+  linkText: string
+  text: string
+}
+
+const Caption: FC<CaptionProps> = ({link, text, linkText}) => {
   return (
     <div className="absolute bottom-4 w-full flex justify-center items-center">
       <p className="text-xs text-teritaryGray md:w-full w-[60vw] text-center">
-        Medication information from the <Link href="https://open.fda.gov/apis/"
+        {text} <Link href={link} 
         className="cursor-pointer underline underline-offset-2 hover:text-white"
-        target="_blank">FDA drug label API</Link>
+        target="_blank">{linkText}</Link>
       </p>
     </div>
   )
