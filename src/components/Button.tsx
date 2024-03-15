75f79781da4349d637c887ebe9d1a506b69dae9a
Yevhenbk <balagutrak.jenia@gmail.com>
2024-03-15 22:02:25 +0100

    add add border

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,9 +12,9 @@ interface ButtonProps {
   disabled?: boolean
 }
 
-const Button: FC<ButtonProps> = (
-    { children, onClick, type, height, width, background, ariaLabel, disabled }
-  ) => {
+const Button: FC<ButtonProps> = ({ 
+  children, onClick, type, height, width, background, ariaLabel, disabled 
+}) => {
   return (
     <button className={classNames("relative flex items-center justify-center rounded-md text-center", 
     "font-medium disabled:hover:cursor-default disabled:opacity-65", {
@@ -22,7 +22,7 @@ const Button: FC<ButtonProps> = (
         height === 12 && background === "white",
       "h-8 text-xs bg-white text-black hover:bg-buttonHover disabled:bg-zinc-50": 
         height === 8 && background === "white",
-      "h-8 text-xs border-[1px] border-secondaryGray hover:bg-secondaryGray": 
+      "h-8 text-xs border border-secondaryGray hover:bg-secondaryGray": 
         height === 8 && background === "transparent",
     })} onClick={onClick} type={type} style={{ width: width}} aria-label={ariaLabel} disabled={disabled}>
       {children}
