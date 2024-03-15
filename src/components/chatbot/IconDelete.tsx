2e91bfbf1ca74efde549e4a7d8ada5ee399ad3c3
Yevhenbk <balagutrak.jenia@gmail.com>
2024-03-15 22:05:08 +0100

    add icon delete

--- src/components/chatbot/IconDelete.tsx
+++ src/components/chatbot/IconDelete.tsx
@@ -1,12 +1,12 @@
-import { FC } from 'react'
+import { FC } from "react"
 import { FaRegTrashAlt } from "react-icons/fa"
-import { deleteChat } from '@/actions/actions'
+import { deleteChat } from "@/actions/actions"
 
 interface IconDeleteProps {
   id: number
 }
 
-const IconDelete: FC<IconDeleteProps> = ({id}) => {
+const IconDelete: FC<IconDeleteProps> = ({ id }) => {
   return (
     <button className="absolute right-0 top-0 w-20 flex items-center justify-center 
     h-[49.6px] border-[1px] border-secondaryGray z-[9] hover:bg-white rounded-e-md
