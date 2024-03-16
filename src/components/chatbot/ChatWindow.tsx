bc54e621064a8f8d5fc0e05ead0b3b0874f25fa6
Yevhenbk <balagutrak.jenia@gmail.com>
2024-03-17 00:53:14 +0100

    add props

--- src/components/chatbot/ChatWindow.tsx
+++ src/components/chatbot/ChatWindow.tsx
@@ -3,14 +3,20 @@
 import { FC, useEffect, useRef } from "react"
 import Message from "./Message"
 import AIResponse from "./AIResponse"
+import Modal from "../Modal"
 import { Message as MessageAIData } from "ai/react"
 import { MessageTypes as MessageAPIData } from "@/utils/messageTypes"
 
 interface ChatWindowProps {
   messages: (MessageAPIData | MessageAIData)[]
+  modalTitle: string
+  modalDescription: string
+  modalResponse: string
 }
 
-const ChatWindow: FC<ChatWindowProps> = ({ messages }) => {
+const ChatWindow: FC<ChatWindowProps> = ({ 
+  messages, modalTitle, modalDescription, modalResponse 
+}) => {
   const chatContainerRef = useRef<HTMLDivElement>(null)
 
   const scrollToBottom = () => {
@@ -30,19 +36,34 @@ const ChatWindow: FC<ChatWindowProps> = ({ messages }) => {
   return (
     <div className="w-[80vw] md:w-[45rem] relative overflow-y-scroll pt-16 pb-6
     mb-28 text-white  h-[-webkit-fill-available]" ref={chatContainerRef}>
-      {messages.map((message) => {
-        if ("question" in message) {
-          const apiMessage = message as MessageAPIData
-          return (
-            <Message key={apiMessage.id} response={apiMessage.response} question={apiMessage.question} />
-          )
-        } else {
-          const aiMessage = message as MessageAIData
-          return (
-            <AIResponse key={aiMessage.id} message={aiMessage} />
-          )
-        }
-      })}
+      {messages.length > 0 ?
+        messages.map((message) => {
+          if ("question" in message) {
+            const apiMessage = message as MessageAPIData
+            return (
+              <Message key={apiMessage.id} response={apiMessage.response} question={apiMessage.question} />
+            )
+          } else {
+            const aiMessage = message as MessageAIData
+            return (
+              <AIResponse key={aiMessage.id} message={aiMessage} />
+            )
+          }
+        }) :
+        <Modal>
+          <div className="flex w-full flex-col gap-4">
+            <h4 className="font-bold text-lg text-white">
+              {modalTitle}
+            </h4>
+            <p className="font-medium text-teritaryGray text-md">
+              {modalDescription}
+            </p>
+            <p className="font-medium text-teritaryGray text-md">
+              {modalResponse}
+            </p>
+          </div>
+        </Modal>
+      }
     </div>
   )
 }
