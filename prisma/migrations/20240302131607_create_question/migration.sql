02b4710a2cde20b268c7ec397a0cb3e264900d86
Yevhenbk <balagutrak.jenia@gmail.com>
2024-03-17 00:10:59 +0100

    migrate

--- prisma/migrations/20240302131607_create_question/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateTable
-CREATE TABLE "Question" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "text" TEXT NOT NULL,
-    "kindeAuthId" TEXT NOT NULL
-);
--- prisma/migrations/20240316230556_/migration.sql
+++ prisma/migrations/20240316230556_/migration.sql
@@ -1,23 +1,14 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Question` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "Question";
-PRAGMA foreign_keys=on;
-
 -- CreateTable
 CREATE TABLE "Chat" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "chatTitle" TEXT NOT NULL
+    "chatTitle" TEXT NOT NULL,
+    "kindeAuthId" TEXT NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "Message" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "kindeAuthId" TEXT NOT NULL,
     "question" TEXT NOT NULL,
     "response" TEXT NOT NULL,
     "chatId" INTEGER NOT NULL,
